9ce26490bd3688edbd7086182ea4de1211bc42f8
Caue Jesus <cs.jesus98@gmail.com>
2025-12-15 16:04:21 -0300

    Create 1011.java

---
+++ Java Solutions/1011.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+     Scanner sc = new Scanner(System.in);
+     
+     final double pi = 3.14159;
+     double raio = sc.nextDouble();
+     double volume = 4.0/3.0 * pi * Math.pow(raio, 3);
+     
+     System.out.printf("VOLUME = %.3f\n",volume);
+     
+     sc.close();
+    }
+}
